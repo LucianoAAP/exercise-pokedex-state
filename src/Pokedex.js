60f70484767c2188bbdf0fbac485b173d8a2ad67
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-24 19:36:57 -0300

    adiciona ex 1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,27 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            pokemonIndex: 0,
+        }
+        this.nextPokemon = this.nextPokemon.bind(this);
+    }
+
+    nextPokemon() {
+        if (this.state.pokemonIndex + 1 > this.props.pokemons.length - 1) {
+            this.setState({ pokemonIndex : 0 });
+        } else {
+            this.setState((previous, _prop) => ({ pokemonIndex: previous.pokemonIndex + 1 }));
+        }
+    }
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon pokemon={ this.props.pokemons[this.state.pokemonIndex] } />
+                <button onClick={this.nextPokemon} >Próximo pokémon</button>
             </div>
         );
     }
