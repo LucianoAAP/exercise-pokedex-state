9ef5f41bc81235e3416ef9f4393a6cd2b2515f6f
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-06-24 21:38:23 -0300

    adiciona ex 2 e um pouco do bonus

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -6,23 +6,43 @@ class Pokedex extends React.Component {
         super();
         this.state = {
             pokemonIndex: 0,
+            pokemonType: 'all',
         }
         this.nextPokemon = this.nextPokemon.bind(this);
+        this.filterPokemons = this.filterPokemons.bind(this);
     }
 
-    nextPokemon() {
-        if (this.state.pokemonIndex + 1 > this.props.pokemons.length - 1) {
-            this.setState({ pokemonIndex : 0 });
+    changeType(type) {
+        this.setState({ pokemonIndex:0, pokemonType: type });
+    }
+
+    filterPokemons(type) {
+        if (type === 'all') {
+            return this.props.pokemons;
+        }
+        return this.props.pokemons.filter((pokemon) => pokemon.type === type);
+    }
+
+    nextPokemon(list) {
+        if (this.state.pokemonIndex + 1 > list.length - 1) {
+            this.setState({ pokemonIndex: 0 });
         } else {
             this.setState((previous, _prop) => ({ pokemonIndex: previous.pokemonIndex + 1 }));
         }
     }
 
     render() {
+        const pokemonList = this.filterPokemons(this.state.pokemonType);
         return (
             <div className="pokedex">
-                <Pokemon pokemon={ this.props.pokemons[this.state.pokemonIndex] } />
-                <button onClick={this.nextPokemon} >Próximo pokémon</button>
+                <Pokemon pokemon={ pokemonList[this.state.pokemonIndex] } />
+                <div>
+                    <button onClick={ () => this.nextPokemon(pokemonList) } >Próximo pokémon</button>
+                    <br/>
+                    <button onClick={ () => this.changeType('all') } >All</button>
+                    <button onClick={ () => this.changeType('Fire') } >Fire</button>
+                    <button onClick={ () => this.changeType('Psychic') } >Psychic</button>
+                </div>
             </div>
         );
     }
